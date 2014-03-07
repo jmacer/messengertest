46839ab41308ffa2986a30abc8fb6f0a8a7e38de
jmacer <james.michael.acer@gmail.com>
2014-03-07 14:20:52 -0500

    Really rough basic layout for the home screen

--- public/index.php
+++ public/index.php
@@ -1,9 +1,55 @@
 <?php
-
-    // configuration
-    require("../includes/config.php");     
-    
-    render("menu.php");        
-
-    
 ?>
+<html>
+
+	<head> 
+	    <title>Messenger</title> 
+	    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+	    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
+	    <<link rel="stylesheet" href="../public/css/stylesheet.css"/>
+	    <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+	    <script src="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.js"></script>
+	    <script src="../public/js/jquery.cookie.js"></script>
+	    
+	</head> 
+	<body> 
+
+	<div data-role="page">
+
+	    <div data-role="header">
+	        <h1>Messenger</h1>
+	    </div><!-- /header -->
+
+	    <div data-role="content">
+	    	<fieldset class="ui-grid-a">
+
+			    <div class="ui-block-a">
+			    	<div class="ui-bar"> <a href="index.php">
+			    		Family </br>
+			    		<img src="img/family.jpg" />
+		    		</a> </div>
+	    		</div>
+
+	    		<div class="ui-block-b">
+			    	<div class="ui-bar"> <a href="index.php">
+			    		Basketball </br>
+			    		<img src="img/team.jpg" />
+		    		</a> </div>
+	    		</div>	    		
+
+			</fieldset>
+
+
+
+			<div data-role="footer" data-position="fixed"> 
+				Copyright &#169; James Acer | Justin Meretab
+			</div> 
+
+		</div><!-- /content -->
+
+	</div><!-- /page -->
+
+	</body>
+
+</html>
+
