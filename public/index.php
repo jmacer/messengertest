a2bba5f18e6a225f882f1b226632cd14f2405a12
jmacer <james.michael.acer@gmail.com>
2014-03-08 21:17:57 -0500

    added some sliding panels

--- public/index.php
+++ public/index.php
@@ -1,25 +1,39 @@
 <?php
 ?>
-<html>
+<html>	
 
 	<head> 
 	    <title>Messenger</title> 
 	    <meta name="viewport" content="width=device-width, initial-scale=1"> 
-	    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
 	    <<link rel="stylesheet" href="../public/css/stylesheet.css"/>
 	    <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
-	    <script src="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.js"></script>
 	    <script src="../public/js/jquery.cookie.js"></script>
+	    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
 	    
-	</head> 
+	</head> 	
+	
 	<body> 
 
-	<div data-role="page">
+	<div data-role="page" id="homescreen">
+
+		<div data-role="panel" data-display="overlay" id="settings-panel-1">
+		    <div class="ui-panel-inner">
+		    	<p>Let's put some settings buttons in here...</p>
+	    	</div>
+		</div><!-- /panel -->
+
+		<div data-role="panel" data-display="overlay" id="new-group-panel" data-position="right">
+		    <div class="ui-panel-inner">
+		    	<p>Let's put some buttons/contacts for making new groups in here...</p>
+	    	</div>
+		</div><!-- /panel -->
 
 	    <div data-role="header">
-	        <a href="index.html" data-role="button" data-icon="gear" data-iconpos="notext">Settings</a>
+	        <a href="#settings-panel-1" data-role="button" data-icon="gear" data-iconpos="notext">Settings</a>
 	        <h1>Messenger</h1>
-	        <a href="index.html" data-role="button" data-icon="plus" data-iconpos="notext">New Group</a>
+	        <a href="#new-group-panel" data-role="button" data-icon="plus" data-iconpos="notext">New Group</a>
 	    </div><!-- /header -->
 
 	    <div data-role="content">
@@ -37,8 +51,7 @@
 			    		Basketball </br>
 			    		<img src="img/team.jpg" />
 		    		</a> </div>
-	    		</div>	    		
-
+	    		</div>
 			</fieldset>
 
 			<fieldset class="ui-grid-a">
@@ -85,6 +98,7 @@
 
 		</div><!-- /content -->
 
+		
 	</div><!-- /page -->
 
 	</body>
